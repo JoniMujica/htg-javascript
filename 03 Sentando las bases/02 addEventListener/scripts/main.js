06df7f2e2df9d95bd5cc60e77d839dbe4e43eb82
JoniMujica <jonimujica3@hotmail.com>
2020-01-13 17:14:13 -0300

    ejercicio 1 realizado

--- 03 Sentando las bases/02 addEventListener/scripts/main.js
+++ 03 Sentando las bases/02 addEventListener/scripts/main.js
@@ -1,6 +1,6 @@
 'use strict';
 
-const formulario = document.querySelector('#contacto');
+/*const formulario = document.querySelector('#contacto');
 const inputNombre = document.querySelector('#input-nombre');
 
 function recibirNombre(event) {
@@ -12,5 +12,32 @@ function interceptarNombre(event) {
   console.log(event.target.value);
 }
 
+formulario.addEventListener('submit', recibirNombre);
+inputNombre.addEventListener('input', interceptarNombre); */
+
+const formulario = document.querySelector('#contacto');
+const inputNombre = document.querySelector('#input-nombre');
+const errores = document.querySelector('.error');
+
+function recibirNombre(event) {
+  console.log("El formulario se envio correctamente")
+  event.preventDefault();
+  console.log(event);
+}
+
+function interceptarNombre(event) {
+  //console.log(event.target.value);
+  if(event.target.value.length<=4 || event.target.value.length>= 50)
+  {
+    errores.style.display = "block";
+    console.log("ERROR: El nombre debe ser entre 5 y 50 caracteres")
+  }else{
+    errores.style.display = "none";
+  }
+  //console.log(event.target.value);*/
+}
+
+
+  
 formulario.addEventListener('submit', recibirNombre);
 inputNombre.addEventListener('input', interceptarNombre);
