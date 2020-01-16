df21809e8a1c2f46266db23351614e42e8d67e08
JoniMujica <jonimujica3@hotmail.com>
2020-01-16 12:08:45 -0300

    agrego los 2 TPs en una branch

--- 03 Sentando las bases/01 querySelector/scripts/main.js
+++ 03 Sentando las bases/01 querySelector/scripts/main.js
@@ -1,1 +1,43 @@
-console.info('Cuando veas éste mensaje en consola, ya habrás terminado ésta parte del ejercicio. Borra ésta línea cuando ya la hayas visto alguna vez en la consola al acceder a ésta página.')
+//console.info('Cuando veas éste mensaje en consola, ya habrás terminado ésta parte del ejercicio. Borra ésta línea cuando ya la hayas visto alguna vez en la consola al acceder a ésta página.')
+
+        
+
+
+
+const comentarios = document.querySelectorAll(".comment")
+for(let i = 0; i< comentarios.length; i++){
+    //comentarios[1]
+    comentarios[2].style.background = "red";
+} 
+
+/*
+const comentarios = document.querySelectorAll("te")
+for(let i = 0; i< comentarios.length; i++){
+    //comentarios[1]
+    if(limite[i]<50){
+
+    }
+} */
+
+let linea = document.querySelectorAll("hr");
+for(let i = 0; i< linea.length; i++){
+    //comentarios[1]
+    linea[i].style.borderColor = "green";
+} 
+
+
+/*
+const limite = document.querySelectorAll(".comment--text");
+for(let i = 0; i<limite.length;i++)
+{
+*/
+
+
+
+/*
+const comentarios = document.querySelectorAll(".comment")
+for(let i = 0; i< comentarios.length; i++){
+    //comentarios[1]
+    comentarios[i].style.background = "red";
+} 
+*/
